32a7da17c981b90811808ccaab3f8535ec1a3410
Arikado77 <carlosarikado19@gmail.com>
2024-11-22 19:12:32 -0600

    HH

---
+++ js/Interacciones.js
@@ -0,0 +1,17 @@
+function irLogin() 
+{
+    window.location.href = 'LogIn.html'; 
+}
+function irInicio(){
+  window.location.href = 'Index.html';  
+}
+function irNosotros()
+{
+  window.location.href = 'Index.html';  
+}
+function registro()
+{
+  window.location.href = 'Registro.html';
+}
+
+
